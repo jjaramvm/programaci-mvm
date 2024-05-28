c1fd323445976581506c60213619b54c95c49062
Joel Jara <113584459+jarasw@users.noreply.github.com>
2024-05-28 15:24:32 +0200

    Add files via upload

---
+++ UF6/Sistema de Gestió d'Inventaris per a un Joc de Rol/GestioInventarisRPG/src/main/java/com/mycompany/gestioinventarisrpg/MainApp.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestioinventarisrpg;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+    }
+}
